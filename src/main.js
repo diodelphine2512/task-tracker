bd4fd72918ed426f21bd9b4dc092c4062837fbee
Evan <evanswartz01@gmail.com>
2022-02-01 13:14:55 -0500

    initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import {createApp} from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
+createApp(App).$mount('#app')
